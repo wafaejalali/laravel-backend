d1b14ca17a92032a968be85803377c784ef7fe5f
wafae jalali <wafaejalali@example.com>
2023-04-16 03:39:20 +0200

    admin

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -8,13 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Admin extends Model
 {
     use HasFactory;
-    protected $table='admin';
-    protected $primaryKey="id_admin";
-    protected $fillable=[
-        'nom',
-        'prenom',
-        'date_de_naissance',
-        'username',
-        'password'
-    ];
+   
 }
