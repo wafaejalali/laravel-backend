c118b608f197dce903f3d2df07d2b7e96089f658
wafae jalali <wafaejalali@example.com>
2023-04-24 00:26:33 +0200

    env

--- app/Models/Vahicule.php
+++ app/Models/Vahicule.php
@@ -9,7 +9,7 @@ class Vahicule extends Model
 {
     use HasFactory;
     protected $table='vahicules';
-    protected $primaryKey="id_vehicule";
+    protected $primaryKey="id_vahicule";
     protected $fillable=[
         'modele',
         'matricule',
